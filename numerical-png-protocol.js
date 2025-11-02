d0ebb0ddc9e1c5fdce3d433823d3117e109b10d1
shiwaku <yohei.shiwaku@gmail.com>
2025-11-02 16:38:20 +0900

    不要なカラーパレットを削除

--- numerical-png-protocol.js
+++ numerical-png-protocol.js
@@ -3,35 +3,7 @@ import { encode as fastPngEncode } from "https://cdn.jsdelivr.net/npm/fast-png@6
 // 数値PNG(RGB) → 浸水深(m)
 const rgb2value = (r, g, b) => (r * 65536 + g * 256 + b) * 0.001;
 
-// パレット色
-/*
-const value2color = (d, ALPHA = 178) => {
-  if (d >= 200) return [165, 0, 38, ALPHA]; // 濃赤
-  if (d >= 150) return [215, 48, 39, ALPHA]; // 赤
-  if (d >= 100) return [252, 141, 89, ALPHA]; // オレンジ
-  if (d >= 80) return [254, 224, 144, ALPHA]; // 黄
-  if (d >= 60) return [224, 243, 248, ALPHA]; // ごく淡水色
-  if (d >= 40) return [171, 217, 233, ALPHA]; // 水色
-  if (d >= 20) return [116, 173, 209, ALPHA]; // 淡青
-  if (d > 0) return [69, 117, 180, ALPHA]; // 青
-  return [0, 0, 0, 0]; // 0 または NoData は透明
-};
-*/
-
-/*
-const value2color = (d, ALPHA = 178) => {
-  if (d >= 200) return [178, 24, 43, ALPHA]; // 深赤
-  if (d >= 150) return [239, 59, 44, ALPHA]; // 赤
-  if (d >= 100) return [253, 174, 97, ALPHA]; // オレンジ
-  if (d >= 80) return [254, 224, 139, ALPHA]; // 黄
-  if (d >= 60) return [217, 239, 245, ALPHA]; // ごく淡水色
-  if (d >= 40) return [166, 206, 227, ALPHA]; // 水色
-  if (d >= 20) return [103, 169, 207, ALPHA]; // 淡青
-  if (d > 0) return [33, 102, 172, ALPHA]; // 青
-  return [0, 0, 0, 0]; // 0 または NoData は透明
-};
-*/
-
+// カラーパレット
 const value2color = (d, ALPHA = 153) => {
   if (d >= 200) return [150, 0, 24, ALPHA]; // 深赤
   if (d >= 150) return [200, 45, 38, ALPHA]; // 赤
